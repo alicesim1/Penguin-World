b2e589137b5c59c20c1974f87d301220f2b3e118
Alice Sim1 <47580273+alicesim1@users.noreply.github.com>
2021-03-23 22:10:20 +0100

    borrado de binarios

--- out/res/basicos.d
+++
@@ -1,2 +0,0 @@
-out/res/basicos.o: res/basicos.res \
-D:/Dropbox/SGDK/PROJECT/PENGUIN-WORLD/res/font_1c.png
--- out/res/logos.d
+++
@@ -1,5 +0,0 @@
-out/res/logos.o: res/logos.res \
-D:/Dropbox/SGDK/PROJECT/PENGUIN-WORLD/res/Logos/disclaimer.png \
-D:/Dropbox/SGDK/PROJECT/PENGUIN-WORLD/res/Logos/sgdk_logo.png \
-D:/Dropbox/SGDK/PROJECT/PENGUIN-WORLD/res/Logos/AliceSim1.png \
-D:/Dropbox/SGDK/PROJECT/PENGUIN-WORLD/res/Logos/logo_Sega.png
